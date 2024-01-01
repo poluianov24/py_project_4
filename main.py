2362c17f8754072c93368fa3f4c6ff39843c429d
poluianov24 <poluianov.vlad.24@gmail.com>
2024-01-01 21:32:13 +0200

    ex-10 done

---
+++ main.py
@@ -0,0 +1,17 @@
+try:
+    s = input('\033[33mВведіть символ для заповнення фігури: ')
+    x = int(input('Введіть довжину сторони квадратап: '))
+    i = j = 0
+    print('\033[35m')
+    while i < x:
+        while j < x:
+            if j + i >= x - 1:
+                print(s, end='  ')
+            else:
+                print(end='   ')
+            j += 1
+        print()
+        j = 0
+        i += 1
+except Exception as e:
+    print(e)
